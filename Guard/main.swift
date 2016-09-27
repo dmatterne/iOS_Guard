f83c0e503be835148eaace80dfbd05136f6cba4f
David Matterne <david.matterne@gmail.com>
2016-09-27 14:14:16 +0200

    Initial Commit

---
+++ Guard/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guard
+//
+//  Created by Stannis Baratheon on 27/09/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
